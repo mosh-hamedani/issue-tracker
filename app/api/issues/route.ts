67b71bccf6eda8a426dde59ed9cda26e0ca3f0bb
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-09-19 09:43:05 -0400

    Build an API for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from 'zod';
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1)
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description }
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
