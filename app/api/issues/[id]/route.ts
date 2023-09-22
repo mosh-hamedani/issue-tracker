0e7c17764aa2c9d85fd8e45d6f98288c610b0c6a
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-09-22 12:34:50 -0400

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -26,3 +26,21 @@ export async function PATCH(
   
   return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(
+  request: NextRequest, 
+  { params }: { params: { id: string }}) {
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) }
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: 'Invalid issue'}, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: issue.id }
+  });
+
+  return NextResponse.json({});
+}
