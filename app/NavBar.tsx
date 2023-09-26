a4a7e5efc8c5d149044b98d61295ba929e83b155
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2023-09-26 08:11:49 -0700

    Add a loading skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from '@/app/components';
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -63,7 +64,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
 
   if (status === "unauthenticated")
     return <Link className="nav-link" href="/api/auth/signin">Login</Link>;
