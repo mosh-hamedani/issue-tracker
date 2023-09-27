404521ac35d8f0ced9ea5a967e053884f2612f74
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2023-09-27 08:56:23 -0700

    Assign an issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,18 +1,23 @@
-'use client'; 
+"use client";
 
-import { User } from '@prisma/client';
-import { Select } from '@radix-ui/themes'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query';
-import { Skeleton } from '@/app/components';
+import { Issue, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@/app/components";
 
-const AssigneeSelect = () => {
-  const { data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () =>
+      axios.get("/api/users").then((res) => res.data),
     staleTime: 60 * 1000, //60s
-    retry: 3
+    retry: 3,
   });
 
   if (isLoading) return <Skeleton />;
@@ -20,18 +25,28 @@ const AssigneeSelect = () => {
   if (error) return null;
 
   return (
-    <Select.Root>
-      <Select.Trigger placeholder='Assign...' />
+    <Select.Root
+      defaultValue={issue.assignedToUserId || ""}
+      onValueChange={(userId) => {
+        axios.patch("/api/issues/" + issue.id, {
+          assignedToUserId: userId || null,
+        });
+      }}
+    >
+      <Select.Trigger placeholder="Assign..." />
       <Select.Content>
-         <Select.Group>
-            <Select.Label>Suggestions</Select.Label>
-            {users?.map(user => (
-              <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
-            )}
-         </Select.Group>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="">Unassigned</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
       </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -29,7 +29,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction="column" gap="4">
-            <AssigneeSelect />
+            <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
