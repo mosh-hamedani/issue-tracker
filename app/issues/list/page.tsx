d767d9a41aa12b4e3ffac210a7261cc1b1136b6f
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2023-09-27 12:49:28 -0700

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -34,10 +34,17 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map(column => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy
   });
 
   return (
