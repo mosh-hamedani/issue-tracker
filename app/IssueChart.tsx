1793931d4025b6554e72cf29d8e7e021443b94a5
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2023-10-02 11:22:29 -0700

    Build the IssueChart component

---
+++ app/IssueChart.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Card } from '@radix-ui/themes';
+import {
+  ResponsiveContainer,
+  BarChart,
+  XAxis,
+  YAxis,
+  Bar,
+} from 'recharts';
+import React from 'react';
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueChart = ({ open, inProgress, closed }: Props) => {
+  const data = [
+    { label: 'Open', value: open },
+    { label: 'In Progress', value: inProgress },
+    { label: 'Closed', value: closed },
+  ];
+
+  return (
+    <Card>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <XAxis dataKey="label" />
+          <YAxis />
+          <Bar
+            dataKey="value"
+            barSize={60}
+            style={{ fill: 'var(--accent-9)' }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
+
+export default IssueChart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from '@/prisma/client';
 import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -13,5 +14,5 @@ export default async function Home() {
     where: { status: 'CLOSED' },
   });
 
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 }
