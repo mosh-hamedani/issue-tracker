508f8c933153ef7942947b3d7f823c06a8b0a31b
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2023-10-02 09:53:57 -0700

    Build the LatestIssue component

---
+++ app/LatestIssues.tsx
@@ -0,0 +1,49 @@
+import prisma from '@/prisma/client';
+import { Avatar, Card, Flex, Heading, Table } from '@radix-ui/themes';
+import React from 'react';
+import { IssueStatusBadge } from './components';
+import Link from 'next/link';
+
+const LatestIssues = async () => {
+  const issues = await prisma.issue.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  return (
+    <Card>
+      <Heading size="4" mb="5">Latest Issues</Heading>
+      <Table.Root>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Flex justify="between">
+                  <Flex direction="column" align="start" gap="2">
+                    <Link href={`/issues/${issue.id}`}>
+                      {issue.title}
+                    </Link>
+                    <IssueStatusBadge status={issue.status} />
+                  </Flex>
+                  {issue.assignedToUser && (
+                    <Avatar
+                      src={issue.assignedToUser.image!}
+                      fallback="?"
+                      size="2"
+                      radius="full"
+                    />
+                  )}
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Card>
+  );
+};
+
+export default LatestIssues;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Pagination from './components/Pagination';
+import LatestIssues from "./LatestIssues";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default function Home() {
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
+    <LatestIssues />
   );
 }
