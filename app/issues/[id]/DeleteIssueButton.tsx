e927cb66bc3975c3d74db6fdd021df2b3a609a28
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2023-10-05 11:01:40 -0700

    Move modal buttons to the right

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -38,7 +38,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
             Are you sure you want to delete this issue? This action cannot be
             undone.
           </AlertDialog.Description>
-          <Flex mt="4" gap="3">
+          <Flex mt="4" gap="3" justify="end">
             <AlertDialog.Cancel>
               <Button variant="soft" color="gray">
                 Cancel
